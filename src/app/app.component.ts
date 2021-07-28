6a90d2defdbe471931714d1fb309c1bbbad2be08
George-Sava <sava_george.alex@yahoo.com>
2021-07-28 12:06:18 -0700

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    {
+      title: 'Neat Tree',
+      imgUrl: 'assets/img/tree.jpeg',
+      username: 'nature',
+      content: 'I saw this neat tree today.'
+    },
+    {
+      title: 'Snowy Mountain',
+      imgUrl: 'assets/img/mountain.jpeg',
+      username: 'mountainlover',
+      content: 'Here is a picture of a snowy mountain.'
+    },
+    {
+      title: 'Mountain Biking',
+      imgUrl: 'assets/img/biking.jpeg',
+      username: 'biking1222',
+      content: 'I did some biking today.'
+    }
+  ]
 }
